024a4ba9a96e78ff45f22c5a997b15b024e19444
Bhanuprakash-b <54404090+Bhanuprakash-b@users.noreply.github.com>
2024-08-24 15:30:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+import validators,streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
+
+
+## sstreamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.subheader('Summarize URL')
+
+
+
+## Get the Groq API Key and url(YT or website)to be summarized
+with st.sidebar:
+    groq_api_key=st.text_input("Groq API Key",value="",type="password")
+
+generic_url=st.text_input("URL",label_visibility="collapsed")
+engine=st.sidebar.selectbox("Select AI model",["llama3-70b-8192","gemma2-9b-it","mixtral-8x7b-32768"])
+## Adjust response parameter
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+rating=st.sidebar.selectbox("please rate your expereince",["1","2","3","4","5"])
+submit=st.sidebar.button("Submit")
+if submit:
+    if rating=="1"or"2" or"3"or "4"or "5":
+        st.sidebar.error("Thank you for the valuable insights")
+
+## Gemma Model USsing Groq API
+llm =ChatGroq(model=engine, groq_api_key=groq_api_key,temperature=temperature)
+
+prompt_template="""
+Provide a best summary of the following content in 300-400 words:
+Content:{text}
+
+"""
+prompt=PromptTemplate(template=prompt_template,input_variables=["text"])
+
+if st.button("Summarize the Content from YT or Website"):
+    ## Validate all the inputs
+    if not groq_api_key.strip() or not generic_url.strip():
+        st.error("Please provide the information to get started")
+    elif not validators.url(generic_url):
+        st.error("Please enter a valid Url. It can may be a YT video utl or website url")
+
+    else:
+        try:
+            with st.spinner("Waiting..."):
+                ## loading the website or yt video data
+                if "youtube.com" in generic_url:
+                    loader=YoutubeLoader.from_youtube_url(generic_url,add_video_info=True)
+                else:
+                    loader=UnstructuredURLLoader(urls=[generic_url],ssl_verify=False,
+                                                 headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+                docs=loader.load()
+
+                ## Chain For Summarization
+                chain=load_summarize_chain(llm,chain_type="stuff",prompt=prompt)
+                output_summary=chain.run(docs)
+
+                st.success(output_summary)
+        except Exception as e:
+            st.exception(f"Exception:{e}")
+                    
